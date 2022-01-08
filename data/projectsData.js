d79a6c55e84eec04b224701be3cdd7bed315ac84
Bharath Kumar Chandramouli <bharath.kumarc@outlook.com>
2022-01-08 13:54:13 +0530

    Switched to tailwind nextjs blog template

---
+++ data/projectsData.js
@@ -0,0 +1,10 @@
+const projectsData = [
+  {
+    title: 'JBehave Extras',
+    description: `Simple utility classes to gather information about your JBehave project and execution`,
+    imgSrc: '/static/images/jbehave-logo.png',
+    href: 'https://github.com/EverWhimsical/jbehave-extras',
+  },
+]
+
+export default projectsData
